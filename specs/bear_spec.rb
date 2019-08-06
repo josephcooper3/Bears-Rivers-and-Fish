156159dfefffe849396d209f9b862b0dc6be1ab2
Joe Cooper <josephcooper3@gmail.com>
2019-08-06 17:14:13 +0100

    Initialized bear class

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -6,5 +6,16 @@ require_relative('../fish')
 
 class BearTest < Minitest::Test
 
+  def setup
+    @bear = Bear.new("Baloo", "Black")
+  end
+
+  def test_bear_has_name
+    assert_equal("Baloo", @bear.name())
+  end
+
+  def test_bear_has_type
+    assert_equal("Black", @bear.type())
+  end
 
 end
